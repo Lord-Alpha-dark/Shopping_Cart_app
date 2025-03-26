49d4ecce7f7aedeb3bde803f6ba9dcc60a20b30d
Anshu Gupta <anshuacro@gmail.com>
2025-03-26 16:47:19 +0530

    provider class updated

---
+++ lib/models/cart.dart
@@ -0,0 +1,10 @@
+import 'package:shopping_cart_app/models/product.dart';
+
+class Cart{
+
+ final Product product;
+  final int quantity;
+
+  Cart({required this.product, required this.quantity});
+    
+}
---
+++ lib/providers/providers.dart
@@ -0,0 +1,22 @@
+import 'package:riverpod/riverpod.dart';
+import 'package:shopping_cart_app/models/cart.dart';
+import 'package:shopping_cart_app/models/product.dart';
+import 'package:shopping_cart_app/services/product_service.dart';
+
+final productServiceProvider= Provider((ref)=>ProductService());
+
+final productProvider= FutureProvider.family<List<Product>?,int>((ref,page)async {
+   final service=ref.watch(productServiceProvider);
+   return service.getProducts(page, 10); 
+});
+
+final cartProvider = StateProvider<List<Cart>>((ref) => []);
+
+final totalPriceProvider = Provider<double>((ref) {
+  final cart = ref.watch(cartProvider);
+  double total = 0; // Start with 0
+  for (var item in cart) {
+    total = total + (item.product.price!*(1-item.product.discountPercentage!/100) * item.quantity);
+  }
+  return total;
+});
